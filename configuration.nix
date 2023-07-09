897a2e4cee6eca589411644e0b6d1067eb92ccd4
javigomezo <javiergomezo@protonmail.com>
2023-07-09 10:02:50 +0200

    feat(configuration): remove home config

--- configuration.nix
+++ configuration.nix
@@ -3,7 +3,6 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      #<home-manager/nixos>
       ./hardware-configuration.nix
     ];
 
@@ -75,29 +74,6 @@
     extraGroups = [ "disk" "networkmanager" "wheel" ];
     packages = with pkgs; [];
   };
-
-  #home-manager.users.javier = { pkgs, ... }: {
-  #  gtk = {
-  #    enable = true;
-  #    theme = {
-  #      name = "Nordic-bluish-accent";
-  #      #package = pkgs.nordic;
-  #    };
-  #  };
-  #  home.stateVersion = "23.05";
-  #  home.packages = [  ];
-  #  home.pointerCursor = {
-  #    gtk.enable = true;
-  #    package = pkgs.bibata-cursors;
-  #    name = "Bibata-Modern-Classic";
-  #    size = 24;
-  #  };
-  #  programs.firefox.package = (pkgs.wrapFirefox.override { libpulseaudio = pkgs.libpressureaudio; }) pkgs.firefox-unwrapped { };
-  #  programs.git = {
-  #    enable = true;
-  #    userName = "javigomezo";
-  #  };
-  #};
 
   hardware.bluetooth.enable = true;
   hardware.pulseaudio.enable = false;
