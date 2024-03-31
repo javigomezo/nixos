1731deb036f7bda100fe86caf861ba95d4552b82
javigomezo <mail@javigomezo.com>
2024-03-31 15:24:23 +0200

    fix(hyprpaper): use monitor module

--- home-manager/desktop/wayland/hyprpaper/default.nix
+++ home-manager/desktop/wayland/hyprpaper/default.nix
@@ -1,16 +1,23 @@
 {
   inputs,
+  lib,
   config,
   pkgs,
   vars,
   ...
-}: {
+}: let
+  mainMonitor =
+    builtins.elemAt (map (
+      m: "${m.name}"
+    ) (lib.filter (m: m.primary) config.monitors))
+    0;
+in {
   home.packages = [pkgs.hyprpaper];
   home.file = {
     ".config/hypr/hyprpaper.conf" = {
       text = ''
         preload = ${config.home.homeDirectory}/.config/hypr/wallpapers/${vars.wallpaper}
-        wallpaper = DP-1,${config.home.homeDirectory}/.config/hypr/wallpapers/${vars.wallpaper}
+        wallpaper = ${mainMonitor},${config.home.homeDirectory}/.config/hypr/wallpapers/${vars.wallpaper}
       '';
     };
     ".config/hypr/wallpapers" = {
