abd9a756e829a8fe51ae5225b0148d89835d9127
javigomezo <mail@javigomezo.com>
2024-04-11 23:15:35 +0200

    feat(y520): add battery conservation mode

--- hosts/y520/power-management.nix
+++ hosts/y520/power-management.nix
@@ -16,4 +16,17 @@
       CPU_MAX_PERF_ON_BAT = 20;
     };
   };
+
+  systemd.tmpfiles.settings = {
+    "ideapad-set-conservation-mode" = {
+      "/sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode" = {
+        "f+" = {
+          group = "root";
+          user = "root";
+          mode = "0644";
+          argument = "1";
+        };
+      };
+    };
+  };
 }
