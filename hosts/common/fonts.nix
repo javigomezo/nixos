fb8056b0cf37e29b1f5f2baf10b78692824d5fab
javigomezo <mail@javigomezo.com>
2024-04-14 17:49:05 +0200

    fix(fonts): format with alejandra

--- hosts/common/fonts.nix
+++ hosts/common/fonts.nix
@@ -1,10 +1,12 @@
-{ inputs, pkgs, ...}:
 {
+  inputs,
+  pkgs,
+  ...
+}: {
   fonts = {
     packages = [
       inputs.private-fonts.packages.x86_64-linux.ComicCodeLigatures
       pkgs.noto-fonts-emoji
-      # font-awesome
       (pkgs.nerdfonts.override {fonts = ["NerdFontsSymbolsOnly"];})
     ];
     fontDir.enable = true;
