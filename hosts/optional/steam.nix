2947f78cbabde73cf5179d26c485c8ac4cce2fb2
javigomezo <mail@javigomezo.com>
2024-05-05 09:26:02 +0200

    clean(steam): remove not needed overlay

--- hosts/optional/steam.nix
+++ hosts/optional/steam.nix
@@ -1,26 +1,4 @@
 {
-  nixpkgs = {
-    config.allowUnfree = true;
-    config.packageOverrides = pkgs: {
-      steam = pkgs.steam.override {
-        extraPkgs = pkgs:
-          with pkgs; [
-            gamescope
-            xorg.libXcursor
-            xorg.libXi
-            xorg.libXinerama
-            xorg.libXScrnSaver
-            libpng
-            libpulseaudio
-            libvorbis
-            stdenv.cc.cc.lib
-            libgdiplus
-            libkrb5
-            keyutils
-          ];
-      };
-    };
-  };
   programs.gamescope.enable = true;
   programs.steam = {
     enable = true;
