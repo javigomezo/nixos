f7de12fb01398190548ddab6405563778993fbda
javigomezo <mail@javigomezo.com>
2024-05-12 13:53:40 +0200

    chore(steam): disable gamescope

--- hosts/optional/steam.nix
+++ hosts/optional/steam.nix
@@ -1,8 +1,8 @@
 {
-  programs.gamescope.enable = true;
+  #programs.gamescope.enable = true;
   programs.steam = {
     enable = true;
-    gamescopeSession.enable = true;
+    #gamescopeSession.enable = true;
     remotePlay.openFirewall = false; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
   };
