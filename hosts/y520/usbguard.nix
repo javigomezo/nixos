d3ca072e675cbc5bdce6218f66755475a29810ca
javigomezo <mail@javigomezo.com>
2024-06-10 23:02:59 +0200

    fix(usbguard): allow bluetooth

--- hosts/y520/usbguard.nix
+++ hosts/y520/usbguard.nix
@@ -1,5 +1,8 @@
 {
   services.usbguard = {
     enable = true;
+    rules = ''
+      allow id 0bda:0821 serial "00e04c000001" name "Bluetooth Radio " hash "Y4cSpbnKd4P20Q1jjrqRuZnQmtPOufG0JMRSWM4xh2s=" parent-hash "jEP/6WzviqdJ5VSeTUY8PatCNBKeaREvo2OqdplND/o=" with-interface { e0:01:01 e0:01:01 e0:01:01 e0:01:01 e0:01:01 e0:01:01 e0:01:01 } with-connect-type "hardwired"
+    '';
   };
 }
