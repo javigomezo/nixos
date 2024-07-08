5b9b97660f053b3604499a6c9aa0635278cd5bf9
Javier Gomez Ortiz <javier.gomez@eviden.com>
2024-07-08 10:12:47 +0000

    feat(waybar): fix ethernet icon

--- home-manager/desktop/wayland/waybar/default.nix
+++ home-manager/desktop/wayland/waybar/default.nix
@@ -79,9 +79,9 @@
         };
         network = {
           format-wifi = " {essid} ({signalStrength}%)";
-          format-ethernet = " {ipaddr}/{cidr}";
-          tooltip-format = " {ifname} via {gwaddr}";
-          format-linked = " {ifname} (No IP)";
+          format-ethernet = "󰈀 {ipaddr}/{cidr}";
+          tooltip-format = "󰈀 {ifname} via {gwaddr}";
+          format-linked = "󰈂 {ifname} (No IP)";
           format-disconnected = "⚠ Disconnected";
           format-alt = "{ifname}= {ipaddr}/{cidr}";
         };
