3c7eb93d5255047c5c924da2fb18a2d1cfc7bab7
javigomezo <mail@javigomezo.com>
2024-08-11 11:43:05 +0200

    chore(steam): add proton-ge

--- hosts/optional/steam.nix
+++ hosts/optional/steam.nix
@@ -1,9 +1,12 @@
-{
+{pkgs, ...}: {
   #programs.gamescope.enable = true;
   programs.steam = {
     enable = true;
-    #gamescopeSession.enable = true;
+    gamescopeSession.enable = true;
     remotePlay.openFirewall = false; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
   };
 }
