6f8bcbee8ea4246119611f43742da0f76f22c3da
javigomezo <mail@javigomezo.com>
2024-09-14 13:26:38 +0200

    chore(hyprland): add setcursor exec

--- home-manager/desktop/wayland/hyprland/execs.nix
+++ home-manager/desktop/wayland/hyprland/execs.nix
@@ -9,6 +9,7 @@
       exec-once = [
         "${lib.getExe config.programs.hyprlock.package}"
         "${pkgs.polkit-kde-agent}/libexec/polkit-kde-authentication-agent-1"
+        "hyprctl setcursor bibata-modern-classic-hyprcursor 24"
       ];
     };
   };
