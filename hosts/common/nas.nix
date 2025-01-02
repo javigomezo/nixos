678f00c7011271646d206be736d7b5850b31bbff
javigomezo <mail@javigomezo.com>
2025-01-02 09:19:50 +0100

    chore(nas): add parameters for enabling/disabling and changing ips

--- hosts/common/nas.nix
+++ hosts/common/nas.nix
@@ -1,21 +1,44 @@
 {
-  fileSystems = {
-    "/mnt/Qbittorrent" = {
-      device = "10.0.0.2:/home/javier/docker-services/qbittorrent/data/downloads";
-      fsType = "nfs";
-      options = ["nfsvers=4.2" "x-systemd.automount" "noauto" "x-systemd.idle-timeout=60"];
+  lib,
+  config,
+  ...
+}: {
+  options.my.nas-mounts = {
+    enable = lib.mkOption {
+      description = "Enables mounting of NAS shares";
+      type = lib.types.bool;
+      default = true;
     };
-
-    "/mnt/TVShows" = {
-      device = "10.0.0.4:/mnt/main_storage/tvshows";
-      fsType = "nfs";
-      options = ["nfsvers=4.2" "x-systemd.automount" "noauto" "x-systemd.idle-timeout=60"];
+    qbittorrent-mount.enable = lib.mkEnableOption {
+      description = "Enables mounting of qbittorrent share";
+      type = lib.types.bool;
+      default = true;
     };
+    nas-ip = lib.mkOption {
+      description = "NAS IP address";
+      type = lib.types.str;
+      default = "10.0.0.4";
+    };
+  };
+  config = lib.mkIf config.my.nas-mounts.enable {
+    fileSystems = {
+      "/mnt/Qbittorrent" = {
+        device = "10.0.0.2:/home/javier/docker-services/qbittorrent/data/downloads";
+        fsType = "nfs";
+        options = ["nfsvers=4.2" "x-systemd.automount" "noauto" "x-systemd.idle-timeout=60"];
+      };
 
-    "/mnt/Movies" = {
-      device = "10.0.0.4:/mnt/main_storage/movies";
-      fsType = "nfs";
-      options = ["nfsvers=4.2" "x-systemd.automount" "noauto" "x-systemd.idle-timeout=60"];
+      "/mnt/TVShows" = {
+        device = "${config.my.nas-mounts.nas-ip}:/mnt/main_storage/tvshows";
+        fsType = "nfs";
+        options = ["nfsvers=4.2" "x-systemd.automount" "noauto" "x-systemd.idle-timeout=60"];
+      };
+
+      "/mnt/Movies" = {
+        device = "${config.my.nas-mounts.nas-ip}:/mnt/main_storage/movies";
+        fsType = "nfs";
+        options = ["nfsvers=4.2" "x-systemd.automount" "noauto" "x-systemd.idle-timeout=60"];
+      };
     };
   };
 }
