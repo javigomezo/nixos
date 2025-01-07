18e3b03645a19158681e8410602109786d8136ba
javigomezo <mail@javigomezo.com>
2025-01-07 12:22:50 +0100

    chore(nas): add seagate mount

--- hosts/common/nas.nix
+++ hosts/common/nas.nix
@@ -14,6 +14,11 @@
       type = lib.types.bool;
       default = true;
     };
+    seagate-mount.enable = lib.mkEnableOption {
+      description = "Enables mounting of qbittorrent share";
+      type = lib.types.bool;
+      default = true;
+    };
     nas-ip = lib.mkOption {
       description = "NAS IP address";
       type = lib.types.str;
@@ -24,6 +29,12 @@
     fileSystems = {
       "/mnt/Qbittorrent" = {
         device = "10.0.0.2:/home/javier/docker-services/qbittorrent/data/downloads";
+        fsType = "nfs";
+        options = ["nfsvers=4.2" "x-systemd.automount" "noauto" "x-systemd.idle-timeout=60"];
+      };
+
+      "/mnt/Seagate" = {
+        device = "10.0.0.2:/mnt/Seagate";
         fsType = "nfs";
         options = ["nfsvers=4.2" "x-systemd.automount" "noauto" "x-systemd.idle-timeout=60"];
       };
