47a739734af5bc81f37cb50e76e4f0d748720c00
javigomezo <mail@javigomezo.com>
2025-02-21 21:33:34 +0100

    fix(keepalived): change priority from string to int

--- hosts/pi3b/vars.nix
+++ hosts/pi3b/vars.nix
@@ -3,7 +3,7 @@
   wallpaper = "astronaut.png";
   dockerVolumes = "/opt/docker-services";
   mainInterface = "wlan0";
-  keepalivedPriority = "100";
+  keepalivedPriority = 100;
   keepalivedSrcIp = "10.0.0.3";
   keepalivedDstIp = "10.0.0.2";
   keepalivedState = "BACKUP";
