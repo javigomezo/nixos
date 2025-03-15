baa55f9eedfb8e19bdc3edd040dc1375b895cebe
javigomezo <mail@javigomezo.com>
2025-03-15 13:53:36 +0100

    chore(nuc8i3beh): add podman autoupdate

--- home-manager/nuc8i3beh.nix
+++ home-manager/nuc8i3beh.nix
@@ -5,6 +5,10 @@
 
   my.stylix.desktop = false;
   xdg.enable = true;
+  services.podman.autoUpdate = {
+    enable = true;
+    onCalendar = "* 55 * * *";
+  };
 
   # You can update home Manager without changing this value. See
   # the home Manager release notes for a list of state version
