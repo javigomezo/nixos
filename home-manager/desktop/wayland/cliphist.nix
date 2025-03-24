bb1fbc6b3e5e6b4ab9c84888b1253aa1f35e97cf
javigomezo <mail@javigomezo.com>
2025-03-24 11:01:27 +0100

    fix(cliphist): fix cliphist not starting at boot

--- home-manager/desktop/wayland/cliphist.nix
+++ home-manager/desktop/wayland/cliphist.nix
@@ -2,4 +2,10 @@
   services.cliphist = {
     enable = true;
   };
+  systemd.user.services.cliphist = {
+    Unit.After = ["graphical-session.target"];
+  };
+  systemd.user.services.cliphist-images = {
+    Unit.After = ["graphical-session.target"];
+  };
 }
