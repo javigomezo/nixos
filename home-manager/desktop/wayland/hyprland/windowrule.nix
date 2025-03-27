237234647e10c7e93b5fa1cb278a23654bfd2fe3
javigomezo <mail@javigomezo.com>
2025-03-27 19:14:55 +0100

    chore(hyprland): update windowrules

--- home-manager/desktop/wayland/hyprland/windowrule.nix
+++ home-manager/desktop/wayland/hyprland/windowrule.nix
@@ -1,36 +1,38 @@
 {
   wayland.windowManager.hyprland.settings = {
     windowrule = [
-      "float, file_progress"
-      "float, confirm"
-      "float, dialog"
-      "float, download"
-      "float, notification"
-      "float, error"
-      "float, splash"
-      "float, confirmreset"
+      "float, class:^(file_progress)"
+      "float, class:^(confirm)"
+      "float, class:^(dialog)"
+      "float, class:^(download)"
+      "float, class:^(notification)"
+      "float, class:^(error)"
+      "float, class:^(splash)"
+      "float, class:^(confirmreset)"
       "float, title:Open File"
       "float, title:branchdialog"
-      "float, file-roller"
-      "fullscreen, wlogout"
+      "float, title:file-roller"
+      "fullscreen, class:^(wlogout)"
       "float, title:wlogout"
       "fullscreen, title:wlogout"
-      "idleinhibit focus, mpv"
-      "idleinhibit fullscreen, firefox"
+      "idleinhibit focus, class:^(mpv)"
+      "idleinhibit fullscreen, class:^(Firefox)"
+      "idleinhibit focus, class:^(obsidian)$"
       "float, title:^(Media viewer)$"
       "float, title:^(Control de volumen)$"
       "float, title:^(Picture-in-Picture)$"
       "size 800 600, title:^(Control de volumen)$"
       "float, title:^(Authentication Required)$"
-      "float, polkit-kde-authentication-agent-1"
-      "center,polkit-kde-authentication-agent-1"
-      "size 500 500, polkit-kde-authentication-agent-1"
-    ];
-    windowrulev2 = [
       "workspace 5,class:^(obsidian)$"
       "animation popin, class:^(thunar)$"
       "opacity 0.82 0.82, class:^(thunar)$"
       "opacity 0.92 0.92, class:^(obsidian)$"
     ];
+    # windowrulev2 = [
+    #   "workspace 5,class:^(obsidian)$"
+    #   "animation popin, class:^(thunar)$"
+    #   "opacity 0.82 0.82, class:^(thunar)$"
+    #   "opacity 0.92 0.92, class:^(obsidian)$"
+    # ];
   };
 }
