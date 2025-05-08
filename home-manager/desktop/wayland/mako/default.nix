ea417bb308fedeba597837465c5e65461a87dad1
javigomezo <mail@javigomezo.com>
2025-05-08 19:58:40 +0200

    fix(mako): rename settings in mako

--- home-manager/desktop/wayland/mako/default.nix
+++ home-manager/desktop/wayland/mako/default.nix
@@ -1,23 +1,26 @@
 {
   services.mako = {
     enable = true;
-    icons = true;
-    defaultTimeout = 5000;
-    maxVisible = 5;
-    sort = "-time";
-    layer = "overlay"; # Above fullscreen
-    anchor = "top-right";
+    settings = {
+      icons = true;
+      default-timeout = 5000;
+      anchor = "top-right";
+      max-visible = 5;
+      sort = "-time";
+      layer = "overlay"; # Above fullscreen
+      border-radius = 8;
+      border-size = 2;
+      margin = 10;
+      padding = 10;
+      width = 350;
+    };
     # Appearance
     #font = "Comic Code Ligatures 11, Inter Medium 11";
-    width = 350;
-    padding = "10";
-    margin = "10";
-    borderSize = 2;
-    borderRadius = 8;
-    extraConfig = ''
-      [urgency=high]
-      border-color=#bf616a
-      ignore-timeout=1
-    '';
+    #borderRadius = 8;
+    # extraConfig = ''
+    #   [urgency=high]
+    #   border-color=#bf616a
+    #   ignore-timeout=1
+    # '';
   };
 }
