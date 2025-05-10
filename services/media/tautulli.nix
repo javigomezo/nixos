b2434c6a8e6ce37c46e095207204a23dcc3fa1ed
javigomezo <mail@javigomezo.com>
2025-05-10 12:30:11 +0200

    fix(tautulli): update plex logs path

--- services/media/tautulli.nix
+++ services/media/tautulli.nix
@@ -13,7 +13,7 @@ in {
         autoStart = true;
         volumes = [
           "${vars.dockerVolumes}/${containerName}/data/config:/config"
-          "${vars.dockerVolumes}/plex/data/library/Library/Application Support/Plex Media Server/Logs:/logs:ro"
+          "/var/lib/plex/Plex Media Server/Logs:/logs:ro"
           "/etc/localtime:/etc/localtime:ro"
         ];
         environment = {
