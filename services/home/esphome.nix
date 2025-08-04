b36c75119b321e8872714f50e50e40a7131b68f1
javigomezo <mail@javigomezo.com>
2025-08-04 12:38:03 +0200

    fix(esphome): update impermanence folder

--- services/home/esphome.nix
+++ services/home/esphome.nix
@@ -6,10 +6,11 @@
   networking.firewall.interfaces.podman0.allowedTCPPorts = lib.mkAfter [config.services.esphome.port];
   services.esphome = {
     enable = true;
+    allowedDevices = [];
   };
   environment.persistence."/persist".directories = lib.mkAfter [
     {
-      directory = "/var/lib/esphome";
+      directory = "/var/lib/private/esphome";
       user = "esphome";
       group = "esphome";
       mode = "u=rwx,g=rx,o=";
