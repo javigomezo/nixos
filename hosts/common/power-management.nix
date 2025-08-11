9398aa21847aeed36b7155c638adfaaa03b106e0
javigomezo <mail@javigomezo.com>
2025-08-11 09:35:27 +0200

    chore(power-management): decrease cpu & gpu undervolt

--- hosts/common/power-management.nix
+++ hosts/common/power-management.nix
@@ -24,7 +24,7 @@
       coreOffset = lib.mkOption {
         description = "Undervolt core offset";
         type = lib.types.int;
-        default = -80;
+        default = -75;
       };
       gpuOffset = lib.mkOption {
         description = "Undervolt gpu offset";
