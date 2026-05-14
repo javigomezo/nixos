2e5578957573a06993e1210955767f9584f883e0
javigomezo <mail@javigomezo.com>
2026-05-14 13:20:52 +0200

    fix(syncthing): set config dir

--- services/network/syncthing.nix
+++ services/network/syncthing.nix
@@ -3,10 +3,17 @@
     enable = true;
     openDefaultPorts = true;
     settings.gui.insecureSkipHostcheck = true;
+    configDir = "/var/lib/syncthing/config";
   };
   environment.persistence."/persist".directories = lib.mkAfter [
     {
       directory = "/var/lib/syncthing";
+      user = "syncthing";
+      group = "syncthing";
+      mode = "u=rwx,g=rx,o=";
+    }
+    {
+      directory = "/var/lib/syncthing/config";
       user = "syncthing";
       group = "syncthing";
       mode = "u=rwx,g=rx,o=";
