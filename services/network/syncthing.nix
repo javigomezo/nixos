5620466ea12fbf8c1a8c965da0eca07e83f4a49c
javigomezo <mail@javigomezo.com>
2026-05-14 20:34:40 +0200

    chore(syncthing): set declarative folders + devices

--- services/network/syncthing.nix
+++ services/network/syncthing.nix
@@ -2,18 +2,31 @@
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
-    settings.gui.insecureSkipHostcheck = true;
-    configDir = "/var/lib/syncthing/config";
+    settings = {
+      gui.insecureSkipHostcheck = true;
+      devices.kindle = {
+        id = "NPNF4EC-ITB4BRE-OMTDQLY-Y426DZE-JLKSBR4-H6A6GMQ-ILYITNT-5CO3TQP";
+        name = "kindle";
+      };
+      folders = {
+        koreader_database = {
+          enable = true;
+          devices = ["kindle"];
+          path = "/var/lib/syncthing/koreader/database";
+          label = "database";
+        };
+        libros = {
+          enable = true;
+          devices = ["kindle"];
+          path = "/var/lib/syncthing/koreader/libros";
+          label = "libros";
+        };
+      };
+    };
   };
   environment.persistence."/persist".directories = lib.mkAfter [
     {
       directory = "/var/lib/syncthing";
-      user = "syncthing";
-      group = "syncthing";
-      mode = "u=rwx,g=rx,o=";
-    }
-    {
-      directory = "/var/lib/syncthing/config";
       user = "syncthing";
       group = "syncthing";
       mode = "u=rwx,g=rx,o=";
